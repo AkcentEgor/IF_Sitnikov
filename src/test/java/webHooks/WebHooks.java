1ee1dbcda0737721ef6dbe10190b61335e6d84e7
AkcentEgor <egor.sitnikov@bk.ru>
2025-02-15 20:59:05 +0300

    Добавлен проперти url

--- src/test/java/webHooks/WebHooks.java
+++ src/test/java/webHooks/WebHooks.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import utils.ConfigReader;
 
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -12,7 +13,7 @@ public class WebHooks {
     @BeforeEach
     public void initBrowser(){
         Configuration.timeout = 15000;
-        Selenide.open("https://edujira.ifellow.ru");
+        Selenide.open(ConfigReader.getProperty("url"));
         getWebDriver().manage().window().maximize();
     }
 
