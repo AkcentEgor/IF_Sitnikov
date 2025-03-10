92ae5478343dbfacbf0ac48177e9df5b0bb45fce
Egor Sitnikov <egor.sitnikov@bk.ru>
2025-03-10 20:01:38 +0300

    Добавлены скрины при падении теста

--- src/test/java/webHooks/WebHooks.java
+++ src/test/java/webHooks/WebHooks.java
@@ -2,6 +2,8 @@ package webHooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import utils.ConfigReader;
@@ -15,6 +17,7 @@ public class WebHooks {
         Configuration.timeout = 15000;
         Selenide.open(ConfigReader.getProperty("url"));
         getWebDriver().manage().window().maximize();
+        SelenideLogger.addListener("AllureListener", new AllureSelenide().screenshots(Boolean.parseBoolean(ConfigReader.getProperty("save.screen"))));
     }
 
     @AfterEach
